5a842dda70928b1dcb3ef794410a7213e02bf0d7
Isaac Anyimiah <iandev096@gmail.com>
2026-01-22 23:40:10 +0300

    refactor: replace Cloudinary configuration with import from server library

--- app/api/sign-cloudinary/route.ts
+++ app/api/sign-cloudinary/route.ts
@@ -1,10 +1,4 @@
-import { v2 as cloudinary } from "cloudinary";
-
-cloudinary.config({
-  cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
+import cloudinary from "@/app/server/lib/cloudinary";
 
 export async function POST(request: Request) {
   const body = await request.json();
